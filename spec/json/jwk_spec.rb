dc5992beff490d4fef7d77e582ba904141b0bc31
nov <nov@matake.jp>
2022-03-17 10:53:15 +0900

    start working on openssl3 support
    it'll take lots of time though..

--- spec/json/jwk_spec.rb
+++ spec/json/jwk_spec.rb
@@ -145,7 +145,11 @@ describe JSON::JWK do
 
     describe 'unknown curve' do
       it do
-        key = OpenSSL::PKey::EC.new('secp112r2').generate_key
+        key = if OpenSSL::OPENSSL_VERSION_NUMBER >= 0x30000000 # OpenSSL 3.0+
+          OpenSSL::PKey::EC.generate('secp112r2')
+        else
+          OpenSSL::PKey::EC.new('secp112r2').generate_key
+        end
         expect do
           JSON::JWK.new key
         end.to raise_error JSON::JWK::UnknownAlgorithm, 'Unknown EC Curve'
